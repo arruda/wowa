3e64828ff97ca975e5c80fe322117e0e0322126e
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2014-07-15 22:14:31 -0300

    fixing adding dummy email backend for now

--- wowa/wowa/settings/env_local.py
+++ wowa/wowa/settings/env_local.py
@@ -18,3 +18,6 @@ MORE_INSTALLED_APPS = (
 
 #settings this for debug tools
 INTERNAL_IPS = ('127.0.0.1', '10.0.2.2')
+
+if DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
