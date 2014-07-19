fadd33e61f0653439866b1d231a2c99c27a44a0e
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2014-07-19 20:39:36 -0300

    rm old stuff from gunicorn start script

--- gunicorn_start.sh
+++ gunicorn_start.sh
@@ -2,7 +2,6 @@
 
 NAME="wowa"                                  # Name of the application
 DJANGODIR=/home/docker/proj/wowa             # Django project directory
-SOCKFILE=/home/docker/proj/run/gunicorn.sock  # we will communicte using this unix socket
 #USER=hello                                        # the user to run as
 #GROUP=webapps                                     # the group to run as
 NUM_WORKERS=3                                     # how many worker processes should Gunicorn spawn
@@ -15,9 +14,6 @@ echo $ENV_SETTINGS
 export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
 export PYTHONPATH=$DJANGODIR:$PYTHONPATH
 
-# Create the run directory if it doesn't exist
-RUNDIR=$(dirname $SOCKFILE)
-test -d $RUNDIR || mkdir -p $RUNDIR
 
 # Start your Django Unicorn
 # Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
