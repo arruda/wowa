b3293d76b82243406123c526ad7c0143e81bc8dd
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2014-07-26 23:34:11 -0300

    adding debug toolbar urls when debug=true

--- wowa/wowa/urls.py
+++ wowa/wowa/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 from django.views.generic import TemplateView
 
 from django.contrib import admin
@@ -21,3 +22,9 @@ urlpatterns = patterns('',
 #
 #
 #)
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += patterns('',
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    )
