2c277582d7902cd39b4a0404a7921503089e25e5
Felipe Arruda Pontes <felipe.arruda.pontes@gmail.com>
2014-11-26 18:33:02 -0200

    removing/commenting unused imports

--- wowa/tracker/views.py
+++ wowa/tracker/views.py
@@ -10,16 +10,13 @@
 from __future__ import absolute_import
 
 # view imports
-from django.contrib import messages
 from django.core.urlresolvers import reverse_lazy
-from django.contrib.auth.decorators import login_required
 from django.contrib.messages.views import SuccessMessageMixin
 from django.http import Http404
-from django.shortcuts import get_object_or_404, redirect
 from braces.views import LoginRequiredMixin
-from django.views.generic import DetailView
-from django.views.generic import RedirectView
-from django.views.generic import UpdateView
+# from django.views.generic import DetailView
+# from django.views.generic import RedirectView
+# from django.views.generic import UpdateView
 from django.views.generic import ListView
 from django.views.generic import CreateView
 from django.views.generic import DeleteView
